449765e150b2645f61978a2d5a3c5b9faef1a26f
Sergei <reyangood@gmail.com>
2023-05-18 04:31:06 +0300

    add new tests for images on websites

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,4 +1,5 @@
 from page_loader import download
+import os
 import pytest
 
 
@@ -30,10 +31,26 @@ def test_page_loader_check_requests_count(dir_for_tests, requests_mock):
 def test_page_loader_check_file(dir_for_tests, requests_mock):
     tmp_path = dir_for_tests
 
-    with open("tests/fixtures/some-page-com.html") as fixture:
+    with open("tests/fixtures/some-simple-page-com.html") as fixture:
         requests_mock.get("https://some_page.com", text=fixture.read())
         download("https://some_page.com", tmp_path)
 
         fixture.seek(0)
         with open(f"{tmp_path}/some-page-com.html") as loaded_page:
+            loaded_page.seek(0)
             assert loaded_page.read() == fixture.read()
+
+
+def test_page_loader_downloaded_images(dir_for_tests, requests_mock):
+    tmp_path = dir_for_tests
+
+    with open("tests/fixtures/some-page-com.html") as fixture, open(
+              'tests/fixtures/python.png', 'br') as image:
+
+        requests_mock.get("https://some_page.com", text=fixture.read())
+        requests_mock.get("https://python.png", content=image.read())
+        download("https://some_page.com", tmp_path)
+
+        new_folder_path = os.path.join(tmp_path, 'some-page-com_files')
+        images = os.listdir(new_folder_path)
+        assert 'python.png' in images
