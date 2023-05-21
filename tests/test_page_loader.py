7115504b114b246f573887d7ee5a3afde6b6d040
Sergei <reyangood@gmail.com>
2023-05-21 05:38:28 +0300

    reverse test back, after complet checks from hexlet

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -32,47 +32,47 @@ def test_page_loader_check_requests_count(dir_for_tests, requests_mock):
     assert requests_mock.call_count == 1
 
 
-# def test_page_loader_download_files(dir_for_tests, requests_mock):
-#    tmp_path = dir_for_tests
-#
-#    with open("tests/fixtures/some-complex-page-com.html") as main, open(
-#        "tests/fixtures/fixtures_for_complex/ru-hexlet-io-assets-professions-python.png",  # noqa E501
-#        "br",
-#    ) as image, open(
-#        "tests/fixtures/fixtures_for_complex/ru-hexlet-io-assets-application.css"  # noqa E501
-#    ) as css, open(
-#        "tests/fixtures/fixtures_for_complex/ru-hexlet-io-packs-js-runtime.js"
-#    ) as js:
-#
-#        requests_mock.get("https://ru.hexlet.io/courses", text=main.read())
-#        requests_mock.get(
-#            "https://ru.hexlet.io/assets/application.css", text=css.read()
-#        )
-#        requests_mock.get(
-#            "https://ru.hexlet.io/assets/professions/python.png",
-#            content=image.read(),
-#        )
-#        requests_mock.get(
-#            "https://ru.hexlet.io/packs/js/runtime.js", text=js.read()
-#        )
-#
-#        download("https://ru.hexlet.io/courses", tmp_path)
-#        directory = os.path.join(tmp_path, "ru-hexlet-io-courses_files")
-#        for f in os.listdir(directory):
-#            file_path = os.path.join(directory, f)
-#            fixture_path = os.path.join(
-#                "tests/fixtures/fixtures_for_complex", f
-#            )
-#
-#            with open(file_path, "rb") as file, open(
-#                fixture_path, "rb"
-#            ) as fixture:
-#                if f == "ru-hexlet-io-assets-professions-python.png":
-#                    continue
-#                assert file.read() == fixture.read()
-#
-#        main_path = os.path.join(tmp_path, "ru-hexlet-io-courses.html")
-#    with open(main_path) as main, open(
-#        "tests/fixtures/some-complex-page-com-after.html", "r"
-#            ) as fixture:
-#        assert main.read() == fixture.read()
+def test_page_loader_download_files(dir_for_tests, requests_mock):
+    tmp_path = dir_for_tests
+
+    with open("tests/fixtures/some-complex-page-com.html") as main, open(
+        "tests/fixtures/fixtures_for_complex/ru-hexlet-io-assets-professions-python.png",  # noqa E501
+        "br",
+    ) as image, open(
+        "tests/fixtures/fixtures_for_complex/ru-hexlet-io-assets-application.css"  # noqa E501
+    ) as css, open(
+        "tests/fixtures/fixtures_for_complex/ru-hexlet-io-packs-js-runtime.js"
+    ) as js:
+
+        requests_mock.get("https://ru.hexlet.io/courses", text=main.read())
+        requests_mock.get(
+            "https://ru.hexlet.io/assets/application.css", text=css.read()
+        )
+        requests_mock.get(
+            "https://ru.hexlet.io/assets/professions/python.png",
+            content=image.read(),
+        )
+        requests_mock.get(
+            "https://ru.hexlet.io/packs/js/runtime.js", text=js.read()
+        )
+
+        download("https://ru.hexlet.io/courses", tmp_path)
+        directory = os.path.join(tmp_path, "ru-hexlet-io-courses_files")
+        for f in os.listdir(directory):
+            file_path = os.path.join(directory, f)
+            fixture_path = os.path.join(
+                "tests/fixtures/fixtures_for_complex", f
+            )
+
+            with open(file_path, "rb") as file, open(
+                fixture_path, "rb"
+            ) as fixture:
+                if f == "ru-hexlet-io-assets-professions-python.png":
+                    continue
+                assert file.read() == fixture.read()
+
+        main_path = os.path.join(tmp_path, "ru-hexlet-io-courses.html")
+    with open(main_path) as main, open(
+        "tests/fixtures/some-complex-page-com-after.html", "r"
+            ) as fixture:
+        assert main.read() == fixture.read()
